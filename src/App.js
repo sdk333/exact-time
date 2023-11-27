ff59f8b4310dc4ce82bf62113698c90f4b061daf
sdk333 <sdk333.ru@yandex.ru>
2023-11-28 01:35:54 +0300

    added basic functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [actualTime, setTime] = useState((Math.floor((Date.now() / (1000 * 60 * 60)) % 24) > 21 ? Math.floor((Date.now() / (1000 * 60 * 60)) % 24) - 21 : Math.floor((Date.now() / (1000 * 60 * 60)) % 24) + 3)  + ":" + (Math.floor((Date.now() / 1000 / 60) % 60) < 10 ? "0" + Math.floor((Date.now() / 1000 / 60) % 60) : Math.floor((Date.now() / 1000 / 60) % 60)) + ":" + (Math.floor((Date.now() / 1000) % 60) < 10 ? "0" + Math.floor((Date.now() / 1000) % 60) : Math.floor((Date.now() / 1000) % 60)))
+    const [style, setStyle] = useState("theme1")
+    const [styleBack, setStyleBack] = useState("App1")
+
+
+    useEffect(() => {
+      const interval = setInterval(() => {
+        setTime((Math.floor((Date.now() / (1000 * 60 * 60)) % 24) > 21 ? Math.floor((Date.now() / (1000 * 60 * 60)) % 24) - 21 : Math.floor((Date.now() / (1000 * 60 * 60)) % 24) + 3)  + ":" + (Math.floor((Date.now() / 1000 / 60) % 60) < 10 ? "0" + Math.floor((Date.now() / 1000 / 60) % 60) : Math.floor((Date.now() / 1000 / 60) % 60)) + ":" + (Math.floor((Date.now() / 1000) % 60) < 10 ? "0" + Math.floor((Date.now() / 1000) % 60) : Math.floor((Date.now() / 1000) % 60)))
+    }, 1000);
+      return () => {
+        clearInterval(interval);
+      };
+    }, []);
+
+    const changeStyle1 = () => {
+      setStyle('theme1')
+    }
+    const changeStyle2 = () => {
+      setStyle('theme2')
+    }
+    const changeStyle3 = () => {
+      setStyle('theme3')
+    }
+    
+    const changeStyleBack1 = () => {
+      setStyleBack('App1')
+    }
+    const changeStyleBack2 = () => {
+      setStyleBack('App2')
+    }
+    const changeStyleBack3 = () => {
+      setStyleBack('App3')
+    }
+
+    return (
+      
+      <div className={styleBack}>
+        {/* {Math.floor((Date.now() / 1000) % 60) % 2 == 0 ? <h1 className='time'>{actualTime}</h1> : <h1 className='time1'>{actualTime}</h1>} */}
+        <h1 className={style}>{actualTime}</h1>
+        <button className='but1' onClick={() => {changeStyle1(); changeStyleBack1()}}>светлая тема</button>
+        <button className='but2' onClick={() => {changeStyle2(); changeStyleBack2()}}>тёмная тема</button>
+        <button className='but3' onClick={() => {changeStyle3(); changeStyleBack3()}}>ещё тема</button>
+      </div>
+    );
 }
 
 export default App;
